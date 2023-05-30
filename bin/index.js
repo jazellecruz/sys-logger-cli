bb521f1afa6f5b445629766584b27b4815b6cad1
jazzcruz <jazellecruz22@gmail.com>
2023-05-30 16:28:24 +0800

    moved command functions to lib/

--- bin/index.js
+++ bin/index.js
@@ -1,47 +1,26 @@
 #!/usr/bin/env node
 
-// MAIN MODULES
 const commander = require("commander");
 const program = new commander.Command();
-const os = require("os"); 
 const chalk = require("chalk");
+const { getSystemInfo, 
+       getOSInfo,
+       getCPUInfo,
+       getMemInfo,
+       getNetInfo} = require("../lib/index");
 
 const log = console.log;
-
-const getSystemInfo = () => {
-  let host = os.hostname(),
-  os_name = os.type(),
-  os_platfrom = os.platform(),
-  os_release = os.release(),
-  os_version = os.version();
-
-  // Standardize the os name of both windows and mac for consistency. 
-  if(os_name.toLowerCase() === "windows_nt") os_name = "Windows";
-  if(os_name.toLowerCase() === "darwin") os_name = "macOS";
-
-  return {
-    host: host,
-    os: {
-      name: os_name,
-      platform: os_platfrom,
-      release: os_release,
-      version: os_version
-    }
-  }
-}
 
 program.command("system")
   .description("get information of system")
   .action(() => {
-    let system = getSystemInfo();
-    log(`${chalk.green("Host")}: ${system.host}`);
-    log(`${chalk.green("OS")}: 
-  Name: ${system.os.name}
-  Platform: ${system.os.platform}
-  Release: ${system.os.release}
-  Version: ${system.os.version}
-    `);
-});
+    let {host_name, sys_uptime} = getSystemInfo();
+    let {os_name, os_platform, os_version, os_release} = getOSInfo();
+    let {cpu_model, cpu_speed, processor_count} = getCPUInfo();
+    let {total_mem, free_mem} = getMemInfo();
+    let {net_family, net_address, net_netmask, net_mac_address} = getNetInfo();
+    
+  }); 
 
 program.parse(process.argv);
 
